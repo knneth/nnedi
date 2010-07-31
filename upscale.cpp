f63b2996dc44fa28e4e3f29056417ff366598a26
Loren Merritt <pengvado@akuvian.org>
2010-07-31 11:47:34 +0000

    2D

--- upscale.cpp
+++ upscale.cpp
@@ -14,9 +14,14 @@ int main(int argc, char **argv)
         return 1;
     }
     CImg<uint8_t> src(argv[1]);
-    CImg<uint8_t> dst(src._width, src._height*2, 1, src._spectrum);
+    CImg<uint8_t> tmp(src._width, src._height*2, 1, src._spectrum);
     for(int c=0; c<src._spectrum; c++)
-        upscale_v(dst.data(0,0,0,c), src.data(0,0,0,c), src._width, src._height, src._width, src._width);
+        upscale_v(tmp.data(0,0,0,c), src.data(0,0,0,c), src._width, src._height, src._width, src._width);
+    tmp.transpose();
+    CImg<uint8_t> dst(tmp._width, tmp._height*2, 1, tmp._spectrum);
+    for(int c=0; c<tmp._spectrum; c++)
+        upscale_v(dst.data(0,0,0,c), tmp.data(0,0,0,c), tmp._width, tmp._height, tmp._width, tmp._width);
+    dst.transpose();
     dst.save(argv[2]);
     return 0;
 }
