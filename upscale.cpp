03c5634b99f76e5bb048402d62061ef010cb2879
Loren Merritt <pengvado@akuvian.org>
2010-08-07 19:17:17 +0000

    switch from turnleft/right back to transpose

--- upscale.cpp
+++ upscale.cpp
@@ -7,22 +7,6 @@ extern "C" {
 }
 using namespace cimg_library;
 
-static void turnleft(CImg<uint8_t> &src)
-{
-    CImg<uint8_t> dst(src._height, src._width, 1, src._spectrum);
-    cimg_forXYC(dst,x,y,c)
-        dst(x,y,0,c) = src(dst._height-1-y, x, 0, c);
-    src = dst;
-}
-
-static void turnright(CImg<uint8_t> &src)
-{
-    CImg<uint8_t> dst(src._height, src._width, 1, src._spectrum);
-    cimg_forXYC(dst,x,y,c)
-        dst(x,y,0,c) = src(y, dst._width-1-x, 0, c);
-    src = dst;
-}
-
 int main(int argc, char **argv)
 {
     if(argc != 3) {
@@ -30,14 +14,14 @@ int main(int argc, char **argv)
         return 1;
     }
     CImg<uint8_t> src(argv[1]);
+    src.transpose();
     CImg<uint8_t> tmp(src._width, src._height*2, 1, src._spectrum);
-    for(int c=0; c<src._spectrum; c++)
+    cimg_forC(src, c)
         upscale_v(tmp.data(0,0,0,c), src.data(0,0,0,c), src._width, src._height, src._width, src._width);
-    turnright(tmp);
+    tmp.transpose();
     CImg<uint8_t> dst(tmp._width, tmp._height*2, 1, tmp._spectrum);
-    for(int c=0; c<tmp._spectrum; c++)
+    cimg_forC(tmp, c)
         upscale_v(dst.data(0,0,0,c), tmp.data(0,0,0,c), tmp._width, tmp._height, tmp._width, tmp._width);
-    turnleft(dst);
     dst.save(argv[2]);
     return 0;
 }
