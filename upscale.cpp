8ce55c471c8faa30e9da4d843ea68f5cd09f7af4
Loren Merritt <pengvado@akuvian.org>
2010-11-16 11:39:13 +0000

    halfpel shift

--- upscale.cpp
+++ upscale.cpp
@@ -6,20 +6,38 @@ extern "C" {
 }
 using namespace cimg_library;
 
+static void upscale(CImg<uint8_t> &dst, CImg<uint8_t> &src)
+{
+    cimg_forC(src, c)
+        nnedi_upscale_2x(dst.data(0,0,0,c), src.data(0,0,0,c), src._width, src._height, dst._width, src._width);
+}
+
 int main(int argc, char **argv)
 {
     int nns = 0;
+    int align = 0;
     if(argc > 1 && sscanf(argv[1], "-%d", &nns))
         argc--, argv++;
+    if(argc > 1 && !strcmp(argv[1], "--align"))
+        argc--, argv++, align=1;
     if(argc != 3) {
-        printf("usage: upscale [-#] in.png out.png\n");
+        printf("usage: upscale [-#] [--align] in.png out.png\n");
         return 1;
     }
+
     nnedi_config(nns);
     CImg<uint8_t> src(argv[1]);
-    CImg<uint8_t> dst(src._width*2, src._height*2, 1, src._spectrum);
-    cimg_forC(src, c)
-        nnedi_upscale_2x(dst.data(0,0,0,c), src.data(0,0,0,c), src._width, src._height, dst._width, src._width);
-    dst.save(argv[2]);
+    CImg<uint8_t> d2(src._width*2, src._height*2, 1, src._spectrum);
+    upscale(d2, src);
+
+    if(align) {
+        d2.rotate(180);
+        CImg<uint8_t> d4(d2._width*2, d2._height*2, 1, d2._spectrum);
+        upscale(d4, d2);
+        cimg_forXYC(d2,x,y,c)
+            d2(x,y,0,c) = d4(d4._width-x*2-1, d4._height-y*2-1, 0,c);
+    }
+
+    d2.save(argv[2]);
     return 0;
 }
