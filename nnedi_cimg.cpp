1b174c23a1a1d4ff1611ddda62ff87445008d542
Loren Merritt <pengvado@akuvian.org>
2012-06-02 20:01:17 +0000

    multithread the planes too

--- nnedi_cimg.cpp
+++ nnedi_cimg.cpp
@@ -1,14 +1,47 @@
 #define cimg_display 0
 #include <CImg.h>
+#include <pthread.h>
+#include <unistd.h>
 #include "nnedi.h"
+#include "tables.h"
 using namespace cimg_library;
+
+typedef struct {
+    nnedi_t *ctx;
+    CImg<uint8_t> *dst, *src;
+    int plane;
+} arg_t;
+
+static void worker(arg_t *a)
+{
+    nnedi_upscale_2x(a->ctx, a->dst->data(0,0,0,a->plane), a->src->data(0,0,0,a->plane), a->src->_width, a->src->_height, a->dst->_width, a->src->_width);
+}
 
 CImg<uint8_t> nnedi_upscale_2x_cimg(CImg<uint8_t> src, int nns, int threads)
 {
-    nnedi_t *ctx = nnedi_config(nns, threads);
-    CImg<uint8_t> dst(src._width*2, src._height*2, src._depth, src._spectrum);
-    cimg_forZC(src,z,c)
-        nnedi_upscale_2x(ctx, dst.data(0,0,z,c), src.data(0,0,z,c), src._width, src._height, dst._width, src._width);
+    if(src._depth != 1)
+        throw "";
+    if(threads <= 0 )
+        threads = sysconf(_SC_NPROCESSORS_ONLN);
+    nnedi_t *ctx;
+    CImg<uint8_t> dst(src._width*2, src._height*2, 1, src._spectrum);
+    if(threads < src._spectrum || src._spectrum == 1) {
+        ctx = nnedi_config(nns, threads);
+        cimg_forC(src, i) {
+            arg_t a = {ctx, &dst, &src, i};
+            worker(&a);
+        }
+    } else {
+        ctx = nnedi_config(nns, (threads+1)/src._spectrum);
+        pthread_t handle[src._spectrum];
+        arg_t arg[src._spectrum];
+        for(int i=0; i<src._spectrum; i++) {
+            arg[i] = (arg_t){ctx, &dst, &src, i};
+            pthread_create(&handle[i], NULL, (void*(*)(void*))worker, &arg[i]);
+        }
+        for(int i=0; i<src._spectrum; i++)
+            pthread_join(handle[i], NULL);
+    }
     free(ctx);
     return dst;
 }
--- tables.h
+++ tables.h
@@ -1,3 +1,28 @@
+#include <inttypes.h>
+
+#define MAX_NNS 256
+
 #define ALIGNED_16(x) __attribute__((aligned(16))) x
+
+struct nnedi_t {
+    int cpu;
+    int threads;
+    int nns, nnsi;
+
+    void (*test_dotproduct)(const int16_t *weightsi, int *dst, const uint8_t *pix, intptr_t stride);
+    void (*test_dotproducts)(const int16_t *weightsi, int (*dst)[4], const uint8_t *pix, intptr_t stride, int width);
+    int  (*test_net_x4)(const float *weightsf, int (*dotp)[4]);
+    void (*scale_nets)(const int16_t *weights, const uint8_t *pix, intptr_t stride, uint8_t *dst, const uint16_t *offsets, int n);
+    int  (*merge_test_neighbors)(uint8_t *dst, uint16_t *retest, uint8_t *row0, uint8_t *row1, uint8_t *row2, int n, int parity);
+    int  (*merge_test_runlength)(uint16_t *retest, uint8_t *src, int n);
+    void (*bicubic)(uint8_t *dst, uint8_t *src, intptr_t stride, int width);
+    void (*transpose)(uint8_t *dst, uint8_t *src, int width, int height, int dstride, int sstride);
+
+    ALIGNED_16(int16_t test_weights_i[48*4]);
+    ALIGNED_16(int16_t test_weights_i_transpose[48*4]);
+    ALIGNED_16(float test_weights_f[64]);
+    ALIGNED_16(int16_t scale_weights[(48*2+4*sizeof(float)/sizeof(int16_t))*MAX_NNS]);
+};
+
 extern const float nnedi_test_weights[];
 extern const float *const nnedi_scale_weights_8x6xN[];
